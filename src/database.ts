20b0544adbcf93de89668c4140a49efcdb275fc4
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2024-02-12 21:36:23 +0000

    setup database connection

---
+++ src/database.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize";
+
+export const sequelize: Sequelize = new Sequelize({
+  database: "peachpay_local",
+  username: "root",
+  password: "root",
+  host: "localhosot",
+  port: 8889,
+  dialect: "mysql",
+  logging: true,
+  dialectOptions: { multipleStatements: true },
+});
+
+export async function databaseConnection(): Promise<void> {
+  try {
+    await sequelize.authenticate();
+  } catch (error) {
+    console.log(`Application -  Unable to connect to database`);
+    console.log(`error in application - ${error}`);
+  }
+}
